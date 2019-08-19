70d8e9d112d77bd6332d895d59716cd6d7671b76
ar012 <a.razzak660@gmail.cm>
2019-08-19 16:57:00 +0600

    selfnetworking grails project

---
+++ grails-app/services/com/selftics/snet/MemberService.groovy
@@ -0,0 +1,11 @@
+package com.selftics.snet
+
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class MemberService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ src/main/groovy/com/selftics/snet/GlobalConfig.groovy
@@ -0,0 +1,8 @@
+package com.selftics.snet
+
+class GlobalConfig {
+    public static final def USER_TYPE = [
+            ADMINISTRATOR: "ADMINISTRATOR",
+            REGULAR_MEBMER: "REGULAR_MEMBER",
+    ]
+}
